22d243329e28cd3941fa8b03023acd6be2fb0830
Alex Tanu <alex.tanu96@gmail.com>
2019-03-13 14:06:32 +0100

    added Includes folder och rensat main foldern

--- login.php
+++ login.php
@@ -1,20 +1,7 @@
 <?php  
- session_start();  
- $host = "localhost";  
- $username = "root";  
- $password = "";  
- $database = "test";  
- $message = "";  
- try  
- {  
-      $connect = new PDO("mysql:host=$host; dbname=$database", $username, $password);  
-      $connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
- }
+ require 'Includes/db.php';
 
- catch(PDOException $error)  
- {  
-      $message = $error->getMessage();  
- } 
+
       if(isset($_POST["login"]))  
       {  
            if(empty($_POST["username"]) || empty($_POST["password"]))  
