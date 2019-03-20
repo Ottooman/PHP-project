6a09bf4e548fa7e6070fc596e9c49cf1e7551fcc
Alex Tanu <alex.tanu96@gmail.com>
2019-03-20 11:25:58 +0100

    style login

--- login.php
+++ login.php
@@ -17,16 +17,29 @@ include_once 'User.php';
  <!DOCTYPE html>  
  <html>  
       <head>  
-            
+      <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
       </head>  
       <body> 
-                <h3 align="">PHP Login </h3><br />  
-                <form method="post" action="login.php">  
-                     Username: <input type="text" name="username">
-                     Password: <input type="password" name="password">
-                     <input type="submit" name="login" value="Log In">
-                </form>  
-           </div>  
-           <br />  
+      <div class="container">
+        <div class="row justify-content-center align-items-center" style="height:100vh">
+            <div class="col-4">
+                <div class="card">
+                    <div class="card-body">
+                        <form action="login.php" method="post">
+                            <div class="form-group">
+                                <input type="text" class="form-control" name="username" placeholder="Username">
+                            </div>
+                            <div class="form-group">
+                                <input type="password" class="form-control" name="password" placeholder="Password">
+                            </div>
+                            <input type="submit" name="login" value="login">
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>  
       </body>  
  </html>  
